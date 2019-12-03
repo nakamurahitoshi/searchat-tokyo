34eca3099941b3ef147a9f644434a2d6c59e45ea
nakamurahitoshi <naka.mura.nmhs@gmail.com>
2019-12-03 17:22:55 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "railways#index"
+  # 最寄駅検索のためDBにアクセスするメソッド
+  get '/stations/search', to: 'stations#search'
   resources :stations, only: [:index, :show] do
     resources :messages, only: [:index, :create, :show]
   end
   resources :railways, only: [:index, :update]
   resources :users, only: [:edit, :update, :create] 
+  
 end
